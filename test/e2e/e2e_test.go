ec2192c3fca2e0c4923a6c8b3aa33ad3097a04bb
Jack Mayo <jackomayo@gmail.com/>
2026-01-04 20:09:56 +0000

    downgrade curl in test to not use https

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -241,7 +241,7 @@ var _ = Describe("Manager", Ordered, func() {
 							"name": "curl",
 							"image": "curlimages/curl:latest",
 							"command": ["/bin/sh", "-c"],
-							"args": ["curl -v -k -H 'Authorization: Bearer %s' https://%s.%s.svc.cluster.local:8443/metrics"],
+							"args": ["curl -v -H 'Authorization: Bearer %s' http://%s.%s.svc.cluster.local:8443/metrics"],
 							"securityContext": {
 								"readOnlyRootFilesystem": true,
 								"allowPrivilegeEscalation": false,
